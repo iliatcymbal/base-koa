b14ae0f9ba51d53c248cac83645cfd2dc45ff0eb
Ilia Tsymbal <iliatcymbal@gmail.com>
2017-10-11 01:15:38 +0300

    Added tasks route

--- app/index.js
+++ app/index.js
@@ -8,13 +8,17 @@ const app = new Koa();
 app.use(koaBody());
 app.use(cors());
 
-const {users} = require('./routes');
+const {users, tasks} = require('./routes');
 
 app.use(_.get('/', ctx => ctx.body = 'Hello Koa user'));
 
 app.use(_.get('/users', users.get));
 app.use(_.get('/users/:id', users.getById));
 app.use(_.post('/user', users.create));
+
+app.use(_.get('/tasks', tasks.get));
+app.use(_.get('/tasks/:id', tasks.getById));
+app.use(_.post('/task', tasks.create));
 
 app.listen(8081);
 console.log('listening on port 8081');
